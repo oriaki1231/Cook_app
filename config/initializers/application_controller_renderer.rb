93c916c1420560e2a5defb4edf3217fb6c2bbfff
oriaki1231 <aki.raliver.1231@icloud.com>
2020-02-29 20:33:47 +0900

    ひとまず完成？

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,7 @@
 #     https: false
 #   )
 # end
+
+
+
+Refile.secret_key = '0405579e3dcdde644e38d464607ba0a54893fe71108d8d8a625a2423806b5a6896919d765c2af6cbfe6f44bf9758982fc7961808a6948e09e5cb2199151d4b34'
